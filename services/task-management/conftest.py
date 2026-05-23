c90d3812d50154475722c4ccaa6cb3961d4509d3
SatpalSinghHere <satpalsinghalive1@gmail.com>
2026-05-23 13:10:21 +0530

    fixed shared path

--- services/task-management/conftest.py
+++ services/task-management/conftest.py
@@ -2,3 +2,8 @@ import sys
 import os
 
 sys.path.insert(0, os.path.dirname(__file__))
+
+# Make the shared package importable when running tests locally
+_shared_path = os.path.join(os.path.dirname(__file__), "..", "..", "shared")
+if os.path.isdir(_shared_path):
+    sys.path.insert(0, os.path.abspath(_shared_path))
